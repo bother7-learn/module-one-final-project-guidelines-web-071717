e0caa51369064a718dc0b46c2af05dabe5d634b4
kewoodh <kesean.woodhouse@gmail.com>
2017-07-31 15:27:41 -0400

    Creating main menu skeleton v0.3 before update to active record based system

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 require_relative '../lib/interface.rb'
 
+
 welcome_message
-menu
+login
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,29 +1,47 @@
 require 'pry'
+require_relative '../app/models/user.rb'
+
+
+# Main menu of the interface that holds all the functions being executed from run.rb.
+# Asks user to sign into account to link reviews, then filter trails by zip or borough.
+@current_user = nil
+
 def welcome_message
-  puts "Welcome to the NYC Trail Recommendation System!"
+  puts 'Welcome to the NYC Hiking Trail System!'
 end
 
-def menu
-  puts " Please select from the following menu:
-  1) Trails Near Me
-  2) Trails By Borough
-  3) Advanced Search
-  4) NYC Trail Facts
-  5) Exit"
-  user_choice = gets.chomp.to_i
-  if valid_menu_choice?(user_choice)
-    action(user_choice)
+def login
+  puts 'Press 1 to login to an existing account, or 2 to create a new account:'
+  user_account_choice = gets.chomp
+  verify_user_choice(user_account_choice)
+  main_menu
+end
+
+def verify_user_choice(user_account_choice)
+  case user_account_choice.to_i
+  when 1
+    puts "Please enter username:"
+    @current_user = gets.chomp
+  when 2
+    puts "Please enter chosen name:"
+    @current_user = gets.chomp
   else
-    menu
+    puts "Invalid Choice."
+    puts
+    login
   end
-
 end
 
-def valid_menu_choice?(choice)
-  menu_options = [1,2,3,4,5]
-  menu_options.include?(choice)
+#=============Main Options====================#
+
+def main_menu
+  puts "Hello #{@current_user}!"
+  puts "Are you searching by (1) Zip Code or (2) Borough?:"
+  search_option = gets.chomp.to_i
+  puts "Please enter search location:"
+  search_input = gets.chomp
+  validate_search(search_option, search_input)
 end
 
-def action(user_choice)
 
-end
+def validate_search()
