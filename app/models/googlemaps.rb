bc6cf525f5453be38ba1304b41c2b93605163cf5
bother7 <github email address>
2017-08-01 17:46:38 -0400

    mergedd

--- app/models/googlemaps.rb
+++ app/models/googlemaps.rb
@@ -14,6 +14,11 @@ DIRECTIONS_KEY = "AIzaSyBDA-1PhyWdIaRCg0lAghiECA04omO42zE"
     y["results"][0]["geometry"]["location"]
   end
 
+  def geodistance(origin, destination)
+    x = RestClient.get "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins#{origin["lat"]},#{origin["lng"]}&destinations=#{destination["lat"]},#{destination["lng"]}&key=#{DISTANCE_KEY}"
+    y = JSON.parse(x)
+  end
+
   def directions(origin, destination)
     hash = {distance: "", duration: "", directions: []}
     x = RestClient.get "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin["lat"]},#{origin["lng"]}&destination=#{destination["lat"]},#{destination["lng"]}&key=#{DIRECTIONS_KEY}"
