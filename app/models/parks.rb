8762999229baee3dbc2bd9e0a04b7a208ede6c98
bother7 <github email address>
2017-08-01 10:55:27 -0400

    added nearby park finder

--- app/models/parks.rb
+++ app/models/parks.rb
@@ -1,3 +1,16 @@
 class Park < ActiveRecord::Base
 has_many :hiking_trails
+
+
+  def self.nearby_parks(zipcodes)
+    park_list = []
+    zipcodes.each do |zip|
+      self.where("zipcode like (?)", "%#{zip}%").each do |park|
+        park_list << park
+      end
+    end
+    park_list
+  end
+
+
 end
