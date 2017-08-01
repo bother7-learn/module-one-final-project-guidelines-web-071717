47d125723c52682c8b6e595b7e702ee17d8926cc
bother7 <github email address>
2017-08-01 14:20:34 -0400

    starting to add reviews

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,6 +1,14 @@
 require_relative 'googlemaps'
 
 class User < ActiveRecord::Base
+has_many :reviews
+has_many :hiking_trails, through: :reviews
+
+
 include GoogleMaps
+
+  def add_review(hiking_trail, desc)
+    Review.new(self, hiking_trail, desc)
+  end
 
 end
