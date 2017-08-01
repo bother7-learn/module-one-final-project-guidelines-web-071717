9a3f1bc36a5f473c2b2f33a302bc2f220a3aaa3f
kewoodh <kesean.woodhouse@gmail.com>
2017-08-01 09:55:41 -0400

    bug fixes

--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -1,14 +1,30 @@
 require_relative 'user_login.rb'
 def main_menu(user)
-  puts "Thank you for logging in #{user.name}!"
   puts "Are you searching by (1) Zip Code or (2) Borough?:"
-  search_option = gets.chomp.to_i
+  search_option = gets.chomp
   puts "Please enter search location:"
   search_input = gets.chomp
-  validate_search(search_option, search_input)
+  validate_search(user, search_option, search_input)
 end
 
 
-def validate_search(search_option, search_input)
- 
+def validate_search(user, search_option, search_input)
+  if search_option == "1" && ZipCodes.find_by(zipcode: search_input)
+    zipcode_search(user)
+  elsif search_option == "2" && ZipCodes.find_by(borough: search_input)
+    borough_search(user)
+  else
+    puts "Invalid search query created..."
+    main_menu(user)
+  end
+end
+
+def zipcode_search(user)
+  puts "I'm searching by zipcode!"
+  exit
+end
+
+def borough_search(user)
+  puts "I'm searching by borough!"
+  exit
 end
