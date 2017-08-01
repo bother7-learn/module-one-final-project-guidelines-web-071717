8c1690234812a9fbbffb3100ac37c304774b23b4
bother7 <github email address>
2017-08-01 13:32:29 -0400

    geolocation and driving directions

--- app/models/googlemaps.rb
+++ app/models/googlemaps.rb
@@ -4,7 +4,7 @@ module GoogleMaps
 
 GEO_KEY = "AIzaSyARIeC1fdPIK5ON7EjAUQHvMZNmgr46i8k"
 DISTANCE_KEY = "AIzaSyDjJZ-xa57splIRRFTL8roJFT7T1btHT6M"
-
+DIRECTIONS_KEY = "AIzaSyBDA-1PhyWdIaRCg0lAghiECA04omO42zE"
   def coordinates(address)
     if address.class == Array
     address = address.join(",")
@@ -15,9 +15,9 @@ DISTANCE_KEY = "AIzaSyDjJZ-xa57splIRRFTL8roJFT7T1btHT6M"
   end
 
   def travel_distance(origin, destination)
-
-
-    x = RestClient.get
+    x = RestClient.get "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin["lat"]},#{origin["lng"]}&destination=#{destination["lat"]},#{destination["lng"]}&key=#{DIRECTIONS_KEY}"
+    y = JSON.parse(x)
+    y["routes"][0]["legs"]
   end
 
 
