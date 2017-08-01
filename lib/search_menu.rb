dd1abc6874134642709ba69f917af7249b1c780a
kewoodh <kesean.woodhouse@gmail.com>
2017-08-01 13:50:12 -0400

    Started fleshing out search menu of search

--- lib/search_menu.rb
+++ lib/search_menu.rb
@@ -2,10 +2,22 @@ require_relative 'main_menu_interface.rb'
 
 def search_menu(user,zipcode)
   puts
-  puts "Would you like to search for a park or trail?"
+  puts "Would you like to search for a (1)park or (2)trail?"
   choice = gets.chomp
-  puts @nearby_parks.length
-  puts @nearby_trails.length
-  puts @borough_parks.length
-  puts @borough_trails.length
+  case choice
+  when "1"
+    park_list(user,zipcode)
+  when "2"
+    trail_list(user, zipcode)
+  else
+    puts "Invalid option, please enter 1 or 2"
+    search_menu(user, zipcode)
+  end
+end
+
+def park_list(user, zipcode)
+  
+end
+
+def trail_list(user, zipcode)
 end
